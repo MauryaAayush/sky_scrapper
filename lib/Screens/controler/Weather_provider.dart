a8bf0fe3fcd6f4bf114ecc492edf754d35b8a5d4
Admin <maayush235@gmail.com>
2024-06-26 16:53:05 +0530

    Radhey Radhey

--- lib/Screens/controler/Weather_provider.dart
+++ lib/Screens/controler/Weather_provider.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:sky_scrapper/Screens/controler/helpers/api_services.dart';
-
-
-
+import '../model/weatherData_Model.dart';
 
 class WeatherProvider extends ChangeNotifier{
   Weather? weather;
