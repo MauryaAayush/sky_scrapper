6be46c4fbae17361715d570c96f9aefe21e64631
Admin <maayush235@gmail.com>
2024-06-26 16:46:04 +0530

    Radhey Radhey

--- lib/Screens/controler/helpers/api_services.dart
+++ lib/Screens/controler/helpers/api_services.dart
@@ -3,17 +3,6 @@ import 'package:http/http.dart' as http;
 
 class ApiServices {
 
-  Future<String?> getData() async {
-    String apiData =
-        'https://api.weatherapi.com/v1/current.json?key=9885c92eb56e4e0596d103301241906&q=surat';
-    Uri uri = Uri.parse(apiData);
-    Response response = await http.get(uri);
-    if (response.statusCode == 200) {
-      print('Api Called');
-      return response.body;
-    } else {
-      return null;
-    }
-  }
+
 
 }
