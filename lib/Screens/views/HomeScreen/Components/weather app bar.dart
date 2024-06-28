60173e363afa5a3ddf41d003248c8ede7dd46aa1
Admin <maayush235@gmail.com>
2024-06-28 08:53:50 +0530

    Radhey Radhey

--- lib/Screens/views/HomeScreen/Components/weather app bar.dart
+++ lib/Screens/views/HomeScreen/Components/weather app bar.dart
@@ -1,3 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../controler/Weather_provider.dart';
 
 
+Row wetherAppBar(BuildContext context, double width) {
+  return Row(
+    children: [
+      SizedBox(
+        height: 80,
+      ),
+      SizedBox(
+        width: width * 0.04,
+      ),
+      Consumer<WeatherProvider>(
+        builder: (context, weatherProvider, child) {
+          return Text(
+            weatherProvider.weather!.locationModal.name.toString(),
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          );
+        },
+      ),
+      SizedBox(
+        width: width * 0.03,
+      ),
+      Spacer(),
+      const Icon(
+        Icons.menu,
+        size: 25,
+        color: Colors.white,
+      ),
+      SizedBox(
+        width: width * 0.06,
+      ),
+      Icon(
+        Icons.settings,
+        size: 25,
+        color: Colors.white,
+      ),
+      SizedBox(
+        width: width * 0.04,
+      ),
+    ],
+  );
+}
